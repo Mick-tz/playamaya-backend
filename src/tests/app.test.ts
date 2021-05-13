a403f7480c0d72627d6a8a08c19ae335a2a64070
chino <mick.tz@hotmail.de>
2021-05-13 14:33:24 -0500

    template test en js

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -2,10 +2,29 @@ const request = require('supertest');
 const app = require('../index');
 
 describe('users server', () => {
-    it('GET /users --> array usuarios', () => {})
-    it('GET /users/:id --> usuario especifico', () => {})
-    it('GET /users/id --> regresa un 404 para un usuario que no existe', () => {})
-    it('POST /users --> agrega un usuario', () => {})
-    it('PUT /users/:id --> modifica un usuario', () => {})
-    it('DELETE /users/:id --> elimina un usuario', () => {})
+    it('GET /api/usuarios --> array usuarios', () => {
+        return request(app)
+            .get('/api/usuarios')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .then((response) => {
+                expect(response.body).toEqual(
+                    expect.arrayContaining(
+                        expect.objectContaining({
+                            username: expect.any(String),
+                            nombres: expect.any(String),
+                            apellidos: expect.any(String),
+                            // TODO: Agregar expect de imagen
+                            // avatar: expect.any(String),
+                        })
+                    )
+                )
+            }
+        )
+    })
+    it('GET /api/usuarios/:id --> usuario especifico', () => {})
+    it('GET /api/usuarios/:id --> regresa un 404 para un usuario que no existe', () => {})
+    it('POST /api/usuarios --> agrega un usuario', () => {})
+    it('PUT /api/usuarios/:id --> modifica un usuario', () => {})
+    it('DELETE /api/usuarios/:id --> elimina un usuario', () => {})
 })
