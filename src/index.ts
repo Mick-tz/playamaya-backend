cc09592378e7edc94fb8d935ee2eb0740ba78603
cupc4ke <miguel.martinez@ciencias.unam.mx>
2021-05-14 09:57:31 -0500

    configuracion basica -1

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,41 @@
 require('dotenv').config()
 const express = require('express');
+import { Request, Response, NextFunction } from 'express';
+const exphbs = require('express-handlebars');
+import path from 'path';
 
-// inits
+// INIT
 const app = express()
 
-// settings
-const PORT = process.env.PORT || 3000
+// SETTINGS
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+ // motor de vistas
+app.engine('.hbs', exphbs({
+    extname: '.hbs', // extension del archivo
+    layoutsDir: path.join(app.get('views'), 'layouts'), // dirección de la carpeta de layouts
+    partialsDir: path.join(app.get('views'), 'partials'), // dirección de la carpeta de layouts
+    helpers: require('./lib/helpers')
+}));
+app.set('view engine', '.hbs');
 
-// middleware
+// MIDDLEWARE
+// para serializar json 
+app.use(express.json())
+// para interpretar datos de formas de html 
+app.use(express.urlencoded({extended: false}));
 
-//routes
 
-// static files
+// ROUTES // TODO: Usar routers
+app.get('/api/usuarios', (req: Request, res: Response) => res.send('hello world'));
 
-// server
-app.listen(PORT, () => {
-    console.log(`listening in port ${PORT}`)
+// STATIC FILES
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// SERVER
+app.listen(app.get('port'), () => {
+    console.log(`listening in port ${app.get('port')}`)
 })
+
+module.exports = app;
