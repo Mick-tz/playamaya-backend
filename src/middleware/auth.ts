43d7967b70f4728341a86ecdb46b5bdbe7ee1abe
cupc4ke <miguel.martinez@ciencias.unam.mx>
2021-05-20 18:09:25 -0500

    comprueba usuario esta autenticado

---
+++ src/middleware/auth.ts
@@ -0,0 +1,15 @@
+import { Login } from '../models/usuario';
+import { Request, Response, NextFunction } from 'express';
+
+export const auth = async (req: any, res: Response, next: NextFunction) => { // TODO: Fix req as any
+    try {
+        const usuario = await Login.findById(req.session.userId);
+        if (!usuario) {
+            throw new Error('Invalid session userId');
+        }
+        req.usuario = usuario;
+        next()
+    } catch (error) {
+        res.status(400).send({ error: 'usuario no autenticado '});
+    }
+}
