c4823f69305ca78bbb7a69bf7e40dd913830607b
cupc4ke <miguel.martinez@ciencias.unam.mx>
2021-05-20 18:12:13 -0500

    routers de login y delete

--- src/routers/usuarios.ts
+++ src/routers/usuarios.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { userController } from '../controllers/usersController'
+import { userController } from '../controllers/usersController';
+import { auth } from '../middleware/auth';
 
 const router = Router();
 
-router.get('/usuarios',  userController.renderIndex)
-router.post('/usuarios', userController.addUser)
-router.get('/usuarios/:id',  userController.renderUser)
+router.get('/usuarios',  userController.renderIndex);
+router.post('/usuarios', userController.addUser);
+router.get('/usuarios/:id',  userController.renderUser);
+router.post('/usuarios/login', userController.loginUser);
+router.delete('/usuarios/me', auth, userController.deleteUser);
 
 module.exports = router;
