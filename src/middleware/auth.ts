c6b1f1cbcf35b88c096b8eb2fbe694e8907d3bf6
cupc4ke <miguel.martinez@ciencias.unam.mx>
2021-05-20 22:53:04 -0500

    autentica usuario con base en la cookie encriptada

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,7 +2,9 @@ import { Login } from '../models/usuario';
 import { Request, Response, NextFunction } from 'express';
 
 export const auth = async (req: any, res: Response, next: NextFunction) => { // TODO: Fix req as any
+    
     try {
+        
         const usuario = await Login.findById(req.session.userId);
         if (!usuario) {
             throw new Error('Invalid session userId');
